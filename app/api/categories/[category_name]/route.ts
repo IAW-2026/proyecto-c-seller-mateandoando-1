80b2b1f4b171ef988208a747eee17f47dc8df0a5
Guadi11 <guadanayla1101@gmail.com>
2026-06-14 23:41:43 -0300

    cambios en api seller

--- app/api/categories/[category_name]/route.ts
+++ app/api/categories/[category_name]/route.ts
@@ -36,20 +36,32 @@ export async function GET(
       },
       include: {
         categoria: true,
+        vendedor: true,
       },
     });
-
-    // 5. Mapeamos el resultado para cumplir con contrato 03-apis.md
-    const formattedItems = productos.map((item) => ({
-      id_item: item.id_item,
+    const formattedItems = productos.map((item) => {
+     // 1. Convertimos los Decimal de Prisma a Number para poder hacer matemática
+      const priceNum = Number(item.price);
+      const discountPriceNum = item.discount_price ? Number(item.discount_price) : null;
+      
+      // 2. Calculamos el porcentaje (si tiene precio de descuento y es menor al original)
+      let discount_percentage = 0;
+      if (discountPriceNum && discountPriceNum < priceNum) {
+        discount_percentage = Math.round(((priceNum - discountPriceNum) / priceNum) * 100);
+      }
+    return {
+      id_item: item.id_item, // Usamos el nombre del campo ID de tu esquema
       name: item.name,
       price: item.price,
-      stock: item.stock,
-      image_url: item.image_url,
+      image_url: item.image_url, // Lo incluimos para que la Buyer App pueda mostrar la foto
+      id_category: item.id_category,
+      category_name: item.categoria.name,
       id_seller: item.id_seller,
-    }));
-
-    return NextResponse.json(formattedItems);
+      seller_name: item.vendedor.name,
+      discount_percentage: discount_percentage
+    };
+  });
+  return NextResponse.json(formattedItems);
 
   } catch (error) {
     console.error("Error al obtener productos por categoría:", error);
